b6ad597e891e10ee0c7d2225f99db4f4b3e44600
User SA_101 <strangealien101@gmail.com>
2024-10-13 00:39:52 +0500

    commit
    Copy

---
+++ js/slider.js
@@ -0,0 +1,10 @@
+
+const swiper = new swiper('.swiper-container', {
+slidesPerView: 3,
+loop: true,
+// навигация
+navigation: {
+nextEl: '.swiper-button-next',
+prevEl: '.swiper-button-prev',
+},
+});
